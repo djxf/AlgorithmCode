50dcba3512b156fee1af2eedf236df447177d0af
djxf <1763380121.qq.com>
2020-05-11 14:47:06 +0800

    0511

--- 算法/TodayCoding0511.java
+++ 算法/TodayCoding0511.java
@@ -1,5 +1,9 @@
 package 算法;
 
+import java.util.Stack;
+
+import com.sun.corba.se.impl.orbutil.graph.Node;
+
 /**
  * 二叉树
  * @author 斗酒相逢
@@ -12,7 +16,45 @@ public class TodayCoding0511 {
 
 	}
 	/**
-	 * 题目：用递归和非递归遍历二叉树
+	 * 题目1：用递归和非递归遍历二叉树
 	 * 先，中，后
 	 */
+	
+	//非递归先序遍历
+	public static void preOrderUnRecur(Node head) {
+		if(head == null) {
+			return;
+		}
+		Stack<Node> stack = new Stack<>();
+		stack.add(head);
+		while(!stack.isEmpty()) {
+			head = stack.pop();
+			System.out.println(head.value);
+			if(head.right != null) {
+				stack.push(head.right);
+			}
+			if(head.left != null) {
+				stack.push(head.left);
+			}
+		}
+	}
+	
+	//非递归中序遍历
+	public static void inOrderUnRecur(Node head) {
+		if(head == null) {
+			return;
+		}
+		Stack<Node> stack = new Stack<>();
+		while(!stack.isEmpty() || head != null) {
+			if(head != null) {
+				stack.push(head);
+				head = head.left;
+			}else {
+				head = stack.pop();
+				System.out.println(head.value);
+				head = head.right;
+			}
+		}
+	}
+	
 }
